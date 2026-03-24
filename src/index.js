23dc74af9be5a75f407e0125bbe2c123a30405eb
Andriy Sydorchuk <programmers130822@gmail.com>
2026-03-24 22:26:05 +0200

    Add tab switching logic

--- src/index.js
+++ src/index.js
@@ -3,6 +3,41 @@ import { initialPageLoad } from './pageload.js';
 import { showMenuPage } from './menu.js';
 import { showContactPage } from './contact.js';
 
-//initialPageLoad();
-//showMenuPage();
-showContactPage();
+initialPageLoad();
+
+document.addEventListener('DOMContentLoaded', () => {
+    const homeBtn = document.querySelector('.home-btn');
+    const menuBtn = document.querySelector('.menu-btn');
+    const contactBtn = document.querySelector('.contact-btn');
+
+    homeBtn.addEventListener('click', () => {
+        toggleButton(homeBtn);
+        clearPage();
+        initialPageLoad();
+    })
+
+    menuBtn.addEventListener('click', () => {
+        toggleButton(menuBtn);
+        clearPage();
+        showMenuPage();
+    })
+
+    contactBtn.addEventListener('click', () => {
+        toggleButton(contactBtn);
+        clearPage();
+        showContactPage();
+    })
+})
+
+function clearPage() {
+    document.querySelector('#content').innerHTML = '';
+}
+
+function toggleButton(btn) {
+    const btns = document.querySelectorAll('.btn');
+    btns.forEach(btn => {
+        btn.classList.remove('active');
+    })
+
+    btn.classList.add('active');
+}
