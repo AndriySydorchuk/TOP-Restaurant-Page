562d21fcd592a85cedfb7700bb43657177f3db93
Andriy Sydorchuk <programmers130822@gmail.com>
2026-03-24 22:05:59 +0200

    Set page to display only contact page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import "./style.css";
 import { initialPageLoad } from './pageload.js';
 import { showMenuPage } from './menu.js';
+import { showContactPage } from './contact.js';
 
 //initialPageLoad();
-showMenuPage();
+//showMenuPage();
+showContactPage();
